96b1c6bb18935619c75a0ac393e1ec3c1109e0a6
thibs12 <t.lamps@groupeonepoint.com>
2024-06-03 18:43:03 +0200

    fix db.

--- dora/rds.tf
+++ dora/rds.tf
@@ -18,15 +18,15 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+
 resource "aws_db_instance" "rds_instance" {
   engine                 = "mysql"
   identifier             = "dora-db"
   allocated_storage      = 20
   storage_type           = "gp2"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   username               = var.db_username
   password               = var.db_password
-  parameter_group_name   = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot    = true
   publicly_accessible    = false
